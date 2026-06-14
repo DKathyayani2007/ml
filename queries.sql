b39499dd6ffa86e201f3eb5a276d20dcca783408
derangulakathyayani2007 <kathyayaniderangula21225@gmail.com>
2026-06-14 08:24:56 +0530

    Added latest SQL queries

--- queries.sql
+++ queries.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CREATE TABLE employee (
     emp_id INTEGER PRIMARY KEY,
     emp_name TEXT,
@@ -44,7 +43,7 @@ SELECT department, COUNT(*) AS total_employees
 FROM employee
 GROUP BY department
 ORDER BY total_employees DESC;
-=======
+
 CREATE TABLE employee (
     emp_id INTEGER PRIMARY KEY,
     emp_name TEXT,
@@ -90,4 +89,91 @@ SELECT department, COUNT(*) AS total_employees
 FROM employee
 GROUP BY department
 ORDER BY total_employees DESC;
->>>>>>> 2a4480e6a4d54ac1b0366964fdde35385dc43775
+
+
+CREATE TABLE EmployeeDetails (
+    emp_no INTEGER PRIMARY KEY,
+    name TEXT,
+    sal REAL,
+    age INTEGER
+);
+
+INSERT INTO EmployeeDetails VALUES (1, 'Liam', 50000, 25);
+INSERT INTO EmployeeDetails VALUES (2, 'Emma', 60000.5, 30);
+INSERT INTO EmployeeDetails VALUES (3, 'Noah', 75000.75, 35);
+INSERT INTO EmployeeDetails VALUES (4, 'Olivia', 45000.25, 28);
+INSERT INTO EmployeeDetails VALUES (5, 'Ethan', 80000, 32);
+INSERT INTO EmployeeDetails VALUES (6, 'Sophia', 65000, 27);
+INSERT INTO EmployeeDetails VALUES (7, 'Mason', 55000.5, 29);
+
+
+SELECT age, SUM(sal)
+FROM EmployeeDetails
+GROUP BY age
+HAVING SUM(sal) > 50000;
+
+SELECT age, AVG(sal) AS Average_Salary
+FROM EmployeeDetails
+GROUP BY age
+HAVING AVG(sal) > 60000;
+
+CREATE TABLE Learner (
+    ROLL_NO INTEGER PRIMARY KEY,
+    Name TEXT,
+    Address TEXT,
+    Age INTEGER
+);
+
+
+INSERT INTO Learner VALUES (1, 'Liam', 'London', 18);
+INSERT INTO Learner VALUES (2, 'Sofia', 'Berlin', 19);
+INSERT INTO Learner VALUES (3, 'Mateo', 'Madrid', 20);
+INSERT INTO Learner VALUES (4, 'Emma', 'Paris', 21);
+INSERT INTO Learner VALUES (5, 'Noah', 'Rome', 24);
+
+
+
+CREATE TABLE LearnerCourse (
+    COURSE_ID INTEGER,
+    ROLL_NO INTEGER
+);
+
+INSERT INTO LearnerCourse VALUES (1, 1);
+INSERT INTO LearnerCourse VALUES (2, 2);
+INSERT INTO LearnerCourse VALUES (3, 3);
+INSERT INTO LearnerCourse VALUES (6, 7);
+
+SELECT LearnerCourse.COURSE_ID,
+       Learner.NAME,
+       Learner.AGE
+FROM Learner
+INNER JOIN LearnerCourse
+ON Learner.ROLL_NO = LearnerCourse.ROLL_NO;
+
+
+SELECT Learner.NAME,
+       LearnerCourse.COURSE_ID
+FROM Learner
+LEFT JOIN LearnerCourse
+ON LearnerCourse.ROLL_NO = Learner.ROLL_NO;
+
+
+SELECT Learner.NAME,
+       LearnerCourse.COURSE_ID
+FROM Learner
+RIGHT JOIN LearnerCourse
+ON LearnerCourse.ROLL_NO = Learner.ROLL_NO;
+
+
+SELECT Learner.NAME,
+       LearnerCourse.COURSE_ID
+FROM Learner
+FULL JOIN LearnerCourse
+ON LearnerCourse.ROLL_NO = Learner.ROLL_NO;
+
+
+SELECT NAME,
+       COURSE_ID
+FROM Learner
+NATURAL JOIN LearnerCourse;
+
